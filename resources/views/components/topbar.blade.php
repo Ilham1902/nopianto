e3e6ad76e7ad5e671e90370a4eadca79adf66ecd
Ilham <ilhamuntung1902@gmail.com>
2023-08-03 21:46:00 +0700

    modified:   resources/views/components/topbar.blade.php modified:   app/Http/Controllers/Admin/AbsensiController.php modified:   app/Http/Controllers/Admin/KaryawanController.php deleted:    public/admin/.browserslistrc deleted:    public/admin/.gitignore deleted:    public/admin/.travis.yml deleted:    public/admin/404.html deleted:    public/admin/LICENSE deleted:    public/admin/PRO_UPGRADE.txt deleted:    public/admin/README.md deleted:    public/admin/blank.html deleted:    public/admin/buttons.html deleted:    public/admin/cards.html deleted:    public/admin/charts.html deleted:    public/admin/forgot-password.html deleted:    public/admin/gulpfile.js deleted:    public/admin/img/undraw_posting_photo.svg deleted:    public/admin/img/undraw_profile.svg deleted:    public/admin/img/undraw_profile_1.svg deleted:    public/admin/img/undraw_profile_2.svg deleted:    public/admin/img/undraw_profile_3.svg deleted:    public/admin/img/undraw_rocket.svg deleted:    public/admin/index.html deleted:    public/admin/login.html deleted:    public/admin/package-lock.json deleted:    public/admin/package.json deleted:    public/admin/register.html deleted:    public/admin/tables.html deleted:    public/admin/utilities-animation.html deleted:    public/admin/utilities-border.html deleted:    public/admin/utilities-color.html deleted:    public/admin/utilities-other.html deleted:    public/barcode/barcode_20230803_163915.png deleted:    public/barcode/barcode_20230803_165329.png deleted:    public/barcode/barcode_20230803_165444.png deleted:    public/barcode/barcode_20230803_165550.jpg deleted:    public/barcode/qr-code (1).png deleted:    public/barcode/qr-code (2).png deleted:    public/barcode/qr-code.png modified:   resources/views/Admin/data_absensi.blade.php modified:   resources/views/Admin/data_karyawan.blade.php modified:   resources/views/components/sidebar.blade.php modified:   routes/web.php app/Http/Controllers/Admin/DataAdminController.php resources/views/Admin/data_admin.blade.php resources/views/Admin/edit_admin.blade.php resources/views/Admin/tambah_admin.blade.php

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -27,14 +27,6 @@
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
-                <a class="dropdown-item" href="{{ url('#') }}">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Settings
-                </a>
-                <a class="dropdown-item" href="{{ url('#') }}">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Activity Log
-                </a>
                 <div class="dropdown-divider"></div>
                 <form action="{{ route('logout') }}" method="post">
                     @csrf
